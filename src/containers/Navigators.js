2f204c19a2905b016d8dac83f52af6f673cc77e3
VJ <v1bhalodia@gmail.com>
2022-04-07 19:18:47 +0530

    first commit

---
+++ src/containers/Navigators.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {StyleSheet, Text, useColorScheme, View} from 'react-native';
+
+const Navigators = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? 'black' : 'white',
+  };
+
+  return (
+    <View style={styles.root}>
+      <Text>This is app</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
+
+export default Navigators;
